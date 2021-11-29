e6a27344e22b977be7fc8935fde22d0521364a16
Raffaella-11 <raffaella.barretta@mail.polimi.it>
2021-11-29 12:09:27 +0100

    Added audioclips to cards

--- Assets/Scripts/DeckManager.cs
+++ Assets/Scripts/DeckManager.cs
@@ -9,7 +9,7 @@ public class DeckManager : MonoBehaviour
 	public static DeckManager instance;
     public Card[] deck;
 	public Card[] fulldeck;
-	private string[] Names_hard= new string[52]{"caffe", "cane", "cappello", "caraffa", "case", "castagna", "cavalli", "cavi", "chela", "chiave", "chiavi", "chiodi", "chitarra", "coccinella", "coccodrillo",  "coda",  "collo",  "colombo",  "corda",  "corvo",  "cozza",  "cuccia", "cuore", "cuscino", "gallina", "gambe", "gambero", "gambo", "ganci", "gatto", "ghepardo", "gheriglio", "ghiacciolo", "ghiande", "ghiro", "gobba", "gomitolo", "gomme", "gorilla", "gregge", "guanto", "gufo", "guinzaglio", "scacchiera", "scarpe", "scatola", "scheletro", "schiuma", "scoiattolo", "scopa", "scorpione", "scudo"};
+	private string[] Names_hard= new string[53]{"caffe", "cane", "cappello", "caraffa", "case", "castagna", "cavalli", "cavi", "chela", "chiave", "chiavi", "chiodi", "chitarra", "coccinella", "coccodrillo",  "coda",  "collo",  "colombo",  "corda",  "corvo",  "cozza",  "cuccia", "cuore", "cuscino", "gallina", "gambe", "gambero", "gambo", "ganci", "gatto", "ghepardo", "gheriglio", "ghiacciolo", "ghiande", "ghiro", "gobba", "gomitolo", "gomme", "gorilla", "gregge", "guanto", "gufo", "guinzaglio", "scacchi", "scacchiera", "scarpe", "scatola", "scheletro", "schiuma", "scoiattolo", "scopa", "scorpione", "scudo"};
 	private string[] Names_soft= new string[21]{"cellula", "cerotto", "cervello", "ciabatta","cicogna", "cielo", "cigno", "cinghiale", "cintura",  "cipolla", "gelato", "gemelli", "gengiva", "genio", "gigante", "ginocchio", "giraffa", "gnomi", "sceriffo", "sciarpa", "scimmia"};
 	
 	public bool allowWordsC;
@@ -34,7 +34,7 @@ public class DeckManager : MonoBehaviour
     // Start is called before the first frame update
     void Start() {
 	    deck = new Card[10];
-        fulldeck = new Card[73];
+        fulldeck = new Card[74];
 		int index=0;
 		for (int i=0; i<21; i++){
 			Card temp = new Card();
@@ -45,11 +45,12 @@ public class DeckManager : MonoBehaviour
 			temp.IsG = Names_soft[i].StartsWith("g");
 			temp.IsSC = Names_soft[i].StartsWith("sc");
 			temp.artwork = Resources.Load<Sprite>(temp.name);
+			temp.clip = Resources.Load<AudioClip>("Audio/" + temp.name);
             fulldeck[index] = temp;
             index++;
             //Debug.Log(Names_soft[i].StartsWith("c");
 		}	
-		for (int i=0; i<52; i++){
+		for (int i=0; i<53; i++){
 			Card temp = new Card();
 			temp.idx = index;
 			temp.name = Names_hard[i];
@@ -58,6 +59,7 @@ public class DeckManager : MonoBehaviour
 			temp.IsG = Names_hard[i].StartsWith("g");
 			temp.IsSC = Names_hard[i].StartsWith("sc");
 			temp.artwork = Resources.Load<Sprite>(temp.name);
+			temp.clip = Resources.Load<AudioClip>("Audio/" + temp.name);
 			fulldeck[index] = temp;
 			index++;
 			//Debug.Log(Names_hard[i].StartsWith("c");
@@ -72,8 +74,8 @@ public class DeckManager : MonoBehaviour
     {
 	    int replacements = UnityEngine.Random.Range(100, 1000);
 	    for (int i=0; i < replacements; i++){
-		    int A = UnityEngine.Random.Range(0, 34);
-		    int B = UnityEngine.Random.Range(0, 34);
+		    int A = UnityEngine.Random.Range(0, 74);
+		    int B = UnityEngine.Random.Range(0, 74);
 
 		    Card a = fulldeck[A];
 		    Card b = fulldeck[B];
@@ -92,7 +94,7 @@ public class DeckManager : MonoBehaviour
 		allowWordsSC = true;
 
 
-		for (int i = 0; i < 11; i++) {
+		for (int i = 0; i < 10; i++) {
 			deck[i] = fulldeck[i];
 		}
 
@@ -142,6 +144,7 @@ public class Card
    	public string name; 
 	public bool soft;
 	public Sprite artwork;
+	public AudioClip clip;
 	public bool IsC;
 	public bool IsG;
 	public bool IsSC;
